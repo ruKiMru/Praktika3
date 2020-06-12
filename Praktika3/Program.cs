61b408ebd7659d1bb5651b1b55845a793e9c56f4
ruKiMru <iliakazakov59@gmail.com>
2020-06-12 18:38:18 +0500

    Добавьте файлы проекта.

---
+++ Praktika3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Praktika3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = 0.5, x2 = -0.5, y1 = 1, y2 = -1, x, y;
+            Console.WriteLine("Введите координату X");
+                x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Y");
+                y = double.Parse(Console.ReadLine());
+
+            if ((x < x1) && (x > x2) && (y < y1) && (y > y2))
+            {
+                Console.WriteLine("Точка попадает в заштрихованную область");
+                Console.ReadKey(true);
+                return;
+            }
+            else if(((x == x1) || (x == x2) && (y == y1) || (y == y2)))
+            {
+                Console.WriteLine("Точка лежит на границе области");
+                Console.ReadKey(true);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Точка не попадает в заштрихованную область");
+                Console.ReadKey(true);
+                return;
+            }
+        }
+    }
+}
